a43ccb07ab975d0c3beaf91b071325020df773de
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-08-08 15:58:40 +0200

    awesomed

--- 1-break-down/trace-block.js
+++ 1-break-down/trace-block.js
@@ -1,4 +1,4 @@
-// ideal for python tutor
+// pytut: https://goo.gl/Qnu9zJ
 
 let input = 12345;
 let expected = 345;
@@ -7,13 +7,10 @@ let output;
 console.log("Number(String(arg).replace(\"12\", \"\"));\n");
 console.log("- " + input + " should become " + expected);
 
-{ //  (num, 12345) -> (num, 345)
+{ //  Number(String(input).replace("12", ""));
 	const step_1 = String(input); 
-	// str: "12345"
 	const step_2 = step_1.replace("12", ""); 
-	// str: "345"
 	const step_3 = Number(step_2); 
-	// num: 345
 	output = step_3;
 };
 
