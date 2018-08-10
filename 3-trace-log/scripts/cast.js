a7d70100100460ebda98eb2703f2a9065cf66560
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-08-10 17:08:48 +0200

    forgiving cast

--- 3-trace-log/scripts/cast.js
+++ 3-trace-log/scripts/cast.js
@@ -1,18 +1,15 @@
-// utility function: necessary, but not the main purpose
-// encapsulated: otherwise you'd have to write this twice
-
 function cast(type, value) {
 
-	if (type == "Number") {
-    	return Number(value);
+    if (type == "number" || type == "Number") {
+        return Number(value);
 
-    } else if (type == "String") {
-    	return value;
+    } else if (type == "string" || type == "String") {
+        return value;
 
-    } else if (type == "Null") {
-    	return null;
+    } else if (type == "null" || type == "Null") {
+        return null;
 
-    } else if (type == "Boolean") {
+    } else if (type == "boolean" || type == "Boolean") {
         if (value === "true") {
             return true;
         } else {
