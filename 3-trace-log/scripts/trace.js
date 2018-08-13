e615c0071643dae29d36476f981194d2adb0fae7
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-08-13 15:50:44 +0200

    cleaner error handling

--- 3-trace-log/scripts/trace.js
+++ 3-trace-log/scripts/trace.js
@@ -6,12 +6,11 @@ function trace(args, expected) {
 	trace.push( args );
 
 	let step_1;
+	trace.push( {op_1: "String(a);"} );
 	try {
 		step_1 = String(args.a);
-		trace.push( {op_1: "String(a);"} );
 		trace.push( {step_1} );
 	} catch (err) {
-		trace.push( {op_1: "String(a);"} );
 		trace.push(err);
 		trace.push( {op_2: "step_1.replace(b, c);"} );
 		trace.push( {op_3: "Number(step_2);"} );
@@ -20,25 +19,23 @@ function trace(args, expected) {
 	};
 
 	let step_2;
+	trace.push( {op_2: "step_1.replace(b, c);"} );
 	try {
 		step_2 = step_1.replace(args.b, args.c);
-		trace.push( {op_2: "step_1.replace(b, c);"} );
 		trace.push( {step_2} );
 	} catch (err) {
-		trace.push( {op_2: "step_1.replace(b, c);"} );
-		trace.push({op_2: err});
+		trace.push(err);
 		trace.push( {op_3: "Number(step_2);"} );
 		trace.push( {expected} );
 		return trace;
 	};
 
 	let step_3;
+	trace.push( {op_3: "Number(step_2);"} );
 	try {
 		step_3 = Number(step_2);
-		trace.push( {op_3: "Number(step_2);"} );
 		trace.push( {actual: step_3} );
 	} catch (err) {
-		trace.push( {op_3: "Number(step_2);"} );
 		trace.push(err);
 		trace.push( {expected} );
 		return trace;
